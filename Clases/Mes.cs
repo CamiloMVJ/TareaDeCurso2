7f20940e41092818f21a48bc45cda86d93b97896
CamiloMVJ <camilo.mayorga65u@std.uni.edu.ni>
2022-10-28 15:43:18 -0600

    a

--- Clases/Mes.cs
+++ Clases/Mes.cs
@@ -34,20 +34,5 @@
             get { return horas; }
             set { horas = (value > 0) ? value : 0; }
         }
-        public enum Meses
-        {
-            Enero = 0,
-            Febrero = 1,
-            Marzo = 2,
-            Abril = 3,
-            Mayo = 4,
-            Junio = 5,
-            Julio = 6,
-            Agosto = 7,
-            Septiembre = 8,
-            Octubre = 9,
-            Noviembre = 10,
-            Diciembre = 11
-        }
     }
 }
